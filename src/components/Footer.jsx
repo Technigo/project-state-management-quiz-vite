ed79e86c94a33c954c5aa4fd5b26c8f0d37ada62
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-23 18:49:24 +0200

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import "./footer.css";
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>Copyright © {new Date().getFullYear()} Your App Name. All rights reserved.<br /><br />
-                By
-                <a href="https://github.com/Cocofnas/" target="_blanc">Alexandra Meija</a>, <a href="https://github.com/lisawh0/" target="_blanc">Lisa Dahlkar</a>, <a href="https://github.com/jforjanice/" target="_blanc">Janice Cheng</a> & <a href="https://github.com/Hang-Nguyen-Vu" target="_blanc">Hang Nguyen</a></p>
+            <p>Copyright © {new Date().getFullYear()} Under the sea. All rights reserved.<br /><br />
+                By:
+                <a href="https://github.com/Cocofnas/" target="_blanc">Alexandra Meija</a>, <a href="https://github.com/lisawh0/" target="_blanc">Lisa Dahlkar</a>, <a href="https://github.com/jforjanice/" target="_blanc">Janice Cheng</a> & <a href="https://github.com/Hang-Nguyen-Vu" target="_blanc">Hang Nguyen</a> @ Technigo</p>
 
         </footer>
     );
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,19 +1,20 @@
 import useQuizStore from "../stores/useQuizStore";
+import "./summary.css";
 
 export const SummaryPage = () => {
-    const answers = useQuizStore((state) => state.answers);
-    const questions = useQuizStore((state) => state.questions);
-  
-    return (
-      <div>
-        <h1>Quiz Summary</h1>
-        {questions.map((question, index) => (
-          <div key={question.id}>
-            <h3>{question.questionText}</h3>
-            <p>Your Answer: {question.options[answers[index]]}</p>
-            <p>Correct Answer: {question.options[question.correctOption]}</p>
-          </div>
-        ))}
-      </div>
-    );
+  const answers = useQuizStore((state) => state.answers);
+  const questions = useQuizStore((state) => state.questions);
+
+  return (
+    <div className="summary">
+      <h2>Quiz Summary</h2>
+      {questions.map((question, index) => (
+        <div key={question.id}>
+          <h3>{question.questionText}</h3>
+          <p>Your Answer: {question.options[answers[index]]}</p>
+          <p>Correct Answer: {question.options[question.correctOption]}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
