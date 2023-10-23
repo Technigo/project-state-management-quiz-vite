f3b2fc83d3a38ec3c05b3e31b5823fb5bc4626f8
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-23 17:44:29 +0200

    some styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>Copyright © {new Date().getFullYear()} Your App Name. All rights reserved.</p>
+            <p>Copyright © {new Date().getFullYear()} Your App Name. All rights reserved.<br /><br />
+                By
+                <a href="https://github.com/Cocofnas/" target="_blanc">Alexandra Meija</a>, <a href="https://github.com/lisawh0/" target="_blanc">Lisa Dahlkar</a>, <a href="https://github.com/jforjanice/" target="_blanc">Janice Cheng</a> & <a href="https://github.com/Hang-Nguyen-Vu" target="_blanc">Hang Nguyen</a></p>
+
         </footer>
     );
 };
