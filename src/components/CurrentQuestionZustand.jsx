bb7ba3c8cf5cb301d1afb0cb07d91e428146a3f7
EvelynDelCarmen <hey@evelyndelcarmen.com>
2023-10-24 12:52:44 +0200

    zustand-first codes

--- src/components/CurrentQuestionZustand.jsx
+++ src/components/CurrentQuestionZustand.jsx
@@ -1,20 +1,40 @@
 import useQuizStore from "../stores/useQuizStore"; // Adjust the path accordingly
 
+
 export const CurrentQuestionZustand = () => {
   const questions = useQuizStore((state) => state.questions);
   const currentQuestionIndex = useQuizStore(
     (state) => state.currentQuestionIndex
   );
-  const question = questions[currentQuestionIndex];
 
-  if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>;
+  const question = questions[currentQuestionIndex];
+  const goToNextQuestion = useQuizStore((state) => state.goToNextQuestion);
+
+  //  if (!question) {
+  //     return <h1>Oh no! I could not find the current question!</h1>;
+  //   }
+
+  if (question === null) {
+    alert("Oh no! I could not find the current question!");
+  } else if (question.options.index !== currentQuestionIndex) {
+    alert("This is not correct!");
+  } else {
+    alert("This is right!");
   }
 
   return (
-    <div className="managed-component">
+    <div className="question-container">
       <h2>Using Zustand</h2>
       <h1>Question: {question.questionText}</h1>
+      <div className="option-container">
+        {question.options.map((option, index) => (
+          <button key={index}>{option}</button>
+        ))}
+
+      </div>
+      <div className="next-question">
+        <button onClick={goToNextQuestion}>Next</button></div>
     </div>
+
   );
 };
--- src/stores/useQuizStore.js
+++ src/stores/useQuizStore.js
@@ -3,16 +3,37 @@ import { create } from "zustand";
 const questions = [
   {
     id: 1,
-    questionText: "Who set the Olympic record for the 100m dash in 2012?",
-    options: ["Usain Bolt", "Justin Gatlin", "Tyson Gay", "Asafa Powell"],
-    correctAnswerIndex: 0,
+    questionText: "When is the surströmming premiere in 2024?",
+    options: ["Monday, July 1", "Saturday, August 10", "Sunday, September 1", "Friday, August 30", "Wednesday, August 14", "Thursday, August 15th"],
+    correctAnswerIndex: 5,
   },
   {
     id: 2,
     questionText:
-      "When was Michael Phelps last named male World Swimmer of the Year?",
-    options: ["2012", "2014", "2016", "2018"],
-    correctAnswerIndex: 2,
+      "A significant holiday in Sweden is Midsummer. Midsummer night was associated with various supernatural beliefs. What was considered health-promoting?",
+    options: ["Drinking hot tea with honey", "Wearing a crown of thorns", "Climbing a tree at midnight", "Rolling naked in the dew on Midsummer's Day", "Singing loudly at dawn", "Kissing a frog in the moonlight"],
+    correctAnswerIndex: 3,
+  },
+  {
+    id: 3,
+    questionText:
+      "Astrid Lindgren is one of Sweden's most famous authors of all time. However, during World War II, she had a top-secret task, what was it?",
+    options: ["She worked on opening mail and censoring information that was deemed potentially revealing about the Swedish defense.", "Coordinating espionage missions", "Serving as a battlefield nurse", "Running a secret underground radio station", "Inventing code words for the military", "Designing camouflage patterns for soldiers"],
+    correctAnswerIndex: 0,
+  },
+  {
+    id: 4,
+    questionText:
+      "When is the first time meatballs are mentioned in Swedish history?",
+    options: ["In a royal decree from 1605", "Meatballs are first mentioned in print in Sweden in Kajsa Warg's cookbook from 1754", "In a travel diary from the 18th century", "In a Shakespearean play set in Sweden", "In a religious manuscript from the 13th century", "In a historical novel from the 19th century"],
+    correctAnswerIndex: 1,
+  },
+  {
+    id: 5,
+    questionText:
+      "How many cars does the king own??",
+    options: ["5 cars", "20 cars", "2 cars", "10 cars", "15 cars", "7 cars"],
+    correctAnswerIndex: 3,
   },
 ];
 
@@ -71,3 +92,5 @@ const useQuizStore = create((set) => ({
 }));
 
 export default useQuizStore;
+
+
