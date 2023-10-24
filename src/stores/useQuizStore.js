f4559569a205d6780d86a595a305d9ec951ba27f
LauraLyckholm <laura.lyckholm@gmail.com>
2023-10-24 07:32:19 +0200

    Added comments to the useQuizStore for easier understanding of it

--- src/stores/useQuizStore.js
+++ src/stores/useQuizStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 
+// The array of questions for the quiz.
 const questions = [
   {
     id: 1,
@@ -97,41 +98,49 @@ const questions = [
   },
 ];
 
+// This is the main state store for the quiz. It contains the variable "questions", an array empty for the answers, a default index of 0 for the "currentQuestionIndex" and a default state of false for the variable "quizOver"
 const useQuizStore = create((set) => ({
   questions,
   answers: [],
   currentQuestionIndex: 0,
   quizOver: false,
 
+  // This function takes a question id and an answer index, validates them, and then updates the answers array with the user's answer.
   submitAnswer: (questionId, answerIndex) => {
     const question = questions.find((q) => q.id === questionId);
 
+    // Throws an error if there is no question found
     if (!question) {
       throw new Error(
         "Could not find question! Check to make sure you are passing the question id correctly."
       );
     }
 
+    // Throws an error if the answerIndex isn't in the array of possible answers
     if (question.options[answerIndex] === undefined) {
       throw new Error(
         `You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
       );
     }
 
+    // State is updated for the answers array. The set-function takes a callback-function as an argument, which receives the current state as a parameter.
     set((state) => ({
+      // Answers array is updated. Creates a new array that includes all the current answers, and adds a new answer to the end of it.
       answers: [
         ...state.answers,
+        // Here comes the new answers object
         {
-          questionId,
-          answerIndex,
-          question,
-          answer: question.options[answerIndex],
-          isCorrect: question.correctAnswerIndex === answerIndex,
+          questionId, // the questions id
+          answerIndex, // the index of the selected answer
+          question, // the question object
+          answer: question.options[answerIndex], // the text of the selected answer, comes from the options using the answerIndex
+          isCorrect: question.correctAnswerIndex === answerIndex, // A boolean indicating whether the selected answer is correct. Determined by comparing the answerIndex with the correctAnswerIndex in the question object.
         },
       ],
     }));
   },
 
+  // This function increments the currentQuestionIndex to move to the next question. If there are no more questions, it sets quizOver to true.
   goToNextQuestion: () => {
     set((state) => {
       if (state.currentQuestionIndex + 1 === state.questions.length) {
@@ -142,6 +151,7 @@ const useQuizStore = create((set) => ({
     });
   },
 
+  // This function resets the answers array, currentQuestionIndex, and quizOver to their initial states.
   restart: () => {
     set({
       answers: [],
@@ -151,4 +161,5 @@ const useQuizStore = create((set) => ({
   },
 }));
 
+// The useQuizStore is exported for use in other parts of the application.
 export default useQuizStore;
