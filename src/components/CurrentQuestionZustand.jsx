f642938eab5ef4e8f95a5d0636b22b178dbc90c1
veronica astrom <v.aastrom@gmail.com>
2023-10-25 17:41:32 +0200

    added progress bar and feedback if the user is right or wrong. Added a summary button when the user has answered 6 questions

--- src/components/CurrentQuestionZustand.jsx
+++ src/components/CurrentQuestionZustand.jsx
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import useQuizStore from "../stores/useQuizStore"; // Adjust the path accordingly
+import "../styling/question.css"
 
 export const CurrentQuestionZustand = () => {
   const questions = useQuizStore((state) => state.questions);
@@ -10,18 +12,41 @@ export const CurrentQuestionZustand = () => {
   const goToNextQuestion = useQuizStore((state) => state.goToNextQuestion);
   const quizOver = useQuizStore((state) => state.quizOver);
 
-  /*const { submitAnswer, goToNextQuestion, restart } = useQuizStore() infinite loop from Diego ?*/
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [isCorrect, setIsCorrect] = useState(null);
+
+
+  // Calculate the progress value and max based on the current question index
+  const totalQuestions = questions.length;
+  const progressValue = currentQuestionIndex + 1; // Add 1 to make it 1-based
+  const progressMax = totalQuestions;
+
+
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
   }
+
+  const handleAnswerSelection = (answerIndex) => {
+    setSelectedAnswer(answerIndex);
+
+    // Check if the selected answer is correct
+    const isAnswerCorrect = question.correctAnswerIndex === answerIndex;
+    setIsCorrect(isAnswerCorrect);
+  };
+
+  useEffect(() => {
+    // Reset selected answer and correctness feedback when moving to the next question
+    setSelectedAnswer(null);
+    setIsCorrect(null);
+  }, [currentQuestionIndex]);
 
   return (
     <div className="managed-component">
       {/* Question tilte */}
       <h1>Question: {question.questionText}</h1>
       {/* radio button answers  and check if it is correct, if it is correct, show the correct emoji✅, wrong emoji❌*/}
-      <div>
+      <div className="radio-button-wrapper">
         {question.options.map(
           (
             option,
@@ -37,22 +62,45 @@ export const CurrentQuestionZustand = () => {
                 className="radio-button"
                 name="radio-btn"
                 value={option}
-                onChange={submitAnswer(question.id, index)}
-              //checked={value === option}
+                onChange={() => handleAnswerSelection(index)}
+                disabled={selectedAnswer !== null} // Disable the radio buttons after selection
               />
-              {option}
+              <p>{option}</p>
+              {selectedAnswer !== null && (
+                <span className="answer-feedback">
+                  {isCorrect && selectedAnswer === index ? '✅' : null}
+                  {!isCorrect && selectedAnswer === index ? '❌' : null}
+                  {question.correctAnswerIndex === index ? `(Correct Answer)` : null}
+                </span>
+              )}
             </label>
           )
         )}
       </div>
       <div className="next-btn-wrapper">
-        <button className="next-btn" onClick={goToNextQuestion}>
-          Next
-        </button>
+        {currentQuestionIndex + 1 === totalQuestions ? (
+          <button className="summary-btn">
+            Summary
+          </button>
+        ) : (
+          <button
+            className="next-btn"
+            onClick={goToNextQuestion}
+            disabled={selectedAnswer === null}
+          >
+            Next
+          </button>
+        )}
       </div>
-      {/* create a progress bar */}
       <div className="progress-wrapper">
-        <progress className="progress-bar" value="65" max="100" />
+        <progress
+          className="progress-bar"
+          value={(progressValue / progressMax) * 100} // Set value as a percentage
+          max="100"
+        />
+        <div className="progress-text">
+          {`${progressValue}/${progressMax}`}
+        </div>
       </div>
     </div>
   );
