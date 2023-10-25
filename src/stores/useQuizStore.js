e1fc8a8d8f78e9b33c7019cde9025eab4b4ce925
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-10-26 00:04:04 +0200

    Removed some answer options and rephrased

--- src/stores/useQuizStore.js
+++ src/stores/useQuizStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 
 const initialQuestions = [
     {
-        text: "When is the surströmming premiere in 2024?",
+        text: "When is the fermented herring premiere in 2024?",
         image: "src/assets/QuizGame_5.jpg",
         options: [
             "July 1",
@@ -11,32 +11,29 @@ const initialQuestions = [
             "September 1",
             "October 3",
             "August 14",
-            "August 15",
         ],
-        correctAnswerIndex: 5,
+        correctAnswerIndex: 4,
         givenAnswerIndex: null,
     },
     {
         text: "A significant holiday in Sweden is Midsummer. Midsummer night was associated with various supernatural beliefs. What was considered health-promoting?",
         image: "src/assets/QuizGame_3.jpg",
         options: [
-            "Drinking hot tea with honey",
             "Wearing a crown of thorns",
             "Climbing a tree at midnight",
             "Rolling naked in the dew on Midsummer's Day",
             "Singing loudly at dawn",
             "Kissing a frog in the moonlight",
         ],
-        correctAnswerIndex: 3,
+        correctAnswerIndex: 2,
         givenAnswerIndex: null,
     },
     {
         text: "Astrid Lindgren is one of Sweden's most famous authors of all time. However, during World War II, she had a top-secret task, what was it?",
         image: "src/assets/QuizGame_7.jpg",
         options: [
-            "She worked on opening mail and censoring information that was deemed potentially revealing about the Swedish defense.",
+            "Opening mail and censoring information that was deemed potentially revealing about the Swedish defense.",
             "Coordinating espionage missions",
-            "Serving as a battlefield nurse",
             "Running a secret underground radio station",
             "Inventing code words for the military",
             "Designing camouflage patterns for soldiers",
@@ -49,11 +46,10 @@ const initialQuestions = [
         image: "src/assets/QuizGame_2.jpg",
         options: [
             "In a royal decree from 1605",
-            "Meatballs are first mentioned in print in Sweden in Kajsa Warg's cookbook from 1754",
+            "In Kajsa Warg's cookbook from 1754",
             "In a travel diary from the 18th century",
             "In a Shakespearean play set in Sweden",
             "In a religious manuscript from the 13th century",
-            "In a historical novel from the 19th century",
         ],
         correctAnswerIndex: 1,
         givenAnswerIndex: null,
@@ -63,13 +59,12 @@ const initialQuestions = [
         image: "src/assets/QuizGame_10.jpg",
         options: [
             "5 cars",
-            "20 cars",
             "2 cars",
             "10 cars",
             "15 cars",
             "7 cars",
         ],
-        correctAnswerIndex: 3,
+        correctAnswerIndex: 2,
         givenAnswerIndex: null,
     },
 ];
