8040000878c44aeeda9349ee9fa37ca8c7b17255
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-25 09:52:17 +0200

    starter code added
    Co-authored-by: VAstrom <VAstrom@users.noreply.github.com>

--- src/components/CurrentQuestionZustand.jsx
+++ src/components/CurrentQuestionZustand.jsx
@@ -6,6 +6,12 @@ export const CurrentQuestionZustand = () => {
     (state) => state.currentQuestionIndex
   );
   const question = questions[currentQuestionIndex];
+  const submitAnswer = useQuizStore((state) => state.submitAnswer);
+  const goToNextQuestion = useQuizStore((state) => state.goToNextQuestion);
+  const quizOver = useQuizStore((state) => state.quizOver);
+  const restart = useQuizStore((state) => state.restart);
+
+  /*const { submitAnswer, goToNextQuestion, restart } = useQuizStore() infinite loop from Diego ?*/
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
@@ -15,6 +21,35 @@ export const CurrentQuestionZustand = () => {
     <div className="managed-component">
       <h2>Using Zustand</h2>
       <h1>Question: {question.questionText}</h1>
+      <div>
+        {question.options.map((option, index) => ( // https://www.geeksforgeeks.org/javascript-index-inside-map-function/
+          <label
+            className="radio-button-label"
+            htmlFor="radio-btn"
+            key={option}>
+            <input
+              type="radio"
+              className="radio-button"
+              name="radio-btn"
+              value={option}
+              onChange={submitAnswer(question.id, index)}
+            //checked={value === option}
+            />
+            {option}
+          </label>
+        ))}
+      </div>
+      <div>
+        <button
+          className="next-btn"
+          onClick={goToNextQuestion}>Next</button>
+      </div>
+      <div>
+        {quizOver && <button
+          className="restart-btn"
+          onClick={restart}>Restart</button>}
+      </div>
+
     </div>
   );
 };
