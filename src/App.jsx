ac097cf6e68a5f8b218d53236e4aa654f2200d51
mirelcac <141237662+mirelcac@users.noreply.github.com>
2023-10-27 01:58:01 +0200

    SummaryPage Start

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { QuizQuestion } from "./components/layout/quiz/QuizQuestion";
 import { QuizMultichoise } from "./components/layout/quiz/QuizMultichoise";
 import { ProgressBar } from "./components/UI/progressBar/ProgressBar";
 import { NextButton } from "./components/UI/nextButton/NextButton";
+import { SummaryPage } from "./components/layout/summaryPage/SummaryPage";
 
 //import { QuizProvider } from "./context/QuizContext";
 
@@ -19,6 +20,9 @@ export const App = () => {
       </section>
       <div className="progressBar">
         <ProgressBar />
+      </div>
+      <div className="summaryPage">
+        <SummaryPage />
       </div>
     </div>
   );
