8298974e60e7466fe2ee666d450a7c5e1d523b0c
veronica astrom <v.aastrom@gmail.com>
2023-10-26 13:45:48 +0200

    merged summary component into the app. Improved styling. Added footer component. liveshare session. Updated quiz code

--- src/components/CurrentQuestionZustand.jsx
+++ src/components/CurrentQuestionZustand.jsx
@@ -1,100 +1,108 @@
-import React, { useState, useEffect } from 'react';
-import useQuizStore from "../stores/useQuizStore"; // Adjust the path accordingly
-import "../styling/question.css"
+import useQuizStore from "../stores/useQuizStore";
+
+import { Summary } from "./Summary";
+import "../styling/question.css";
+
 
 export const CurrentQuestionZustand = () => {
   const questions = useQuizStore((state) => state.questions);
+  // console.log("Questions is", questions);
+
   const currentQuestionIndex = useQuizStore(
     (state) => state.currentQuestionIndex
   );
   const question = questions[currentQuestionIndex];
-  const goToNextQuestion = useQuizStore((state) => state.goToNextQuestion);
+  // console.log(question);
+  const submitAnswer = useQuizStore((state) => state.submitAnswer);
+  // console.log(submitAnswer);
+  const answers = useQuizStore((state) => state.answers);
+  // console.log("Answers: ⭐️", answers);
+  const answer = answers.find((a) => a.questionId === question.id);
+  // console.log("answer...here...", answer);
+
+  const quizOver = useQuizStore((state) => state.quizOver);
+  // console.log("QuizOver Answer:", quizOver);
 
 
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [isCorrect, setIsCorrect] = useState(null);
+   // Define isCorrect at the top of the component
+   const isCorrect = answer ? answer.isCorrect : false;
 
-
+   
   // Calculate the progress value and max based on the current question index
   const totalQuestions = questions.length;
   const progressValue = currentQuestionIndex + 1; // Add 1 to make it 1-based
   const progressMax = totalQuestions;
 
+  
+  const handleSubmitAnswer = (event) => {
+    const answerIndex = Number(event.target.value);
+    // console.log(answerIndex);
+    const isCorrect = answerIndex === question.correctAnswerIndex;
+    submitAnswer(question.id, answerIndex, isCorrect);
+  };
 
-
+  const handleNextQuestion = () => {
+    if (answer === null) {
+      return; // Do nothing if no answer has been selected
+    }
+    useQuizStore.getState().goToNextQuestion();
+  };
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
   }
-
-  const handleAnswerSelection = (answerIndex) => {
-    setSelectedAnswer(answerIndex);
-
-    // Check if the selected answer is correct
-    const isAnswerCorrect = question.correctAnswerIndex === answerIndex;
-    setIsCorrect(isAnswerCorrect);
-  };
-
-  useEffect(() => {
-    // Reset selected answer and correctness feedback when moving to the next question
-    setSelectedAnswer(null);
-    setIsCorrect(null);
-  }, [currentQuestionIndex]);
-
   return (
-    <div className="managed-component">
-      {/* Question tilte */}
-      <h1>Question {question.id}: {question.questionText}</h1>
-      {/* radio button answers  and check if it is correct, if it is correct, show the correct emoji✅, wrong emoji❌*/}
-      <div className="radio-button-wrapper">
-        {question.options.map(
-          (
-            option,
-            index // https://www.geeksforgeeks.org/javascript-index-inside-map-function/
-          ) => (
-            <label
-              className="radio-button-label"
-              htmlFor={`option-${question.id}-${index}`}
-              key={option}
+    <>
+      {quizOver ? (
+        <section className="summary-wrapper">
+          <Summary answers={answers} />
+        </section>
+      ) : (
+        <section className="managed-component">
+          <h1 className="question-title">
+            Question {currentQuestionIndex + 1}: {question.questionText}
+          </h1>
+
+          {question.options.map((option, index) => {
+            const isSelected = answer && answer.answerIndex === index;
+            const isCorrectOption = question.correctAnswerIndex === index;
+
+            return (
+              <form key={index}>
+                <div className="radio-button-wrapper">
+                <input
+                  type="radio"
+                  id={`option${index}`}
+                  name="option"
+                  required
+                  value={index}
+                  onChange={handleSubmitAnswer}
+                  checked={answer=== option}
+                  disabled={answer}
+                />
+                <label htmlFor={`option${index}`}>
+                  <p>{option}</p>
+                  {isSelected && (
+                    <span className="answer-feedback">
+                      {isCorrectOption ? '✅ Correct Answer' : '❌ Wrong Answer'}
+                      {isCorrect ? null : ` (Correct Answer: ${question.options[question.correctAnswerIndex]})`}
+                    </span>
+                  )}
+                </label>
+                </div>
+              </form>
+            );
+          })}
+
+          <div className="nextBtn">
+            <button
+              type="button"
+              onClick={handleNextQuestion}
+              disabled={!answer}
             >
-              <input
-                type="radio"
-                className="radio-button"
-                name="radio-btn"
-                id={`option-${question.id}-${index}`}
-                value={option}
-                onChange={() => handleAnswerSelection(index)}
-                disabled={selectedAnswer !== null} // Disable the radio buttons after selection
-              />
-              <p>{option}</p>
-              {selectedAnswer !== null && (
-                <span className="answer-feedback">
-                  {isCorrect && selectedAnswer === index ? '✅' : null}
-                  {!isCorrect && selectedAnswer === index ? '❌' : null}
-                  {question.correctAnswerIndex === index ? `(Correct Answer)` : null}
-                </span>
-              )}
-            </label>
-          )
-        )}
-      </div>
-      <div className="next-btn-wrapper">
-        {currentQuestionIndex + 1 === totalQuestions ? (
-          <button className="summary-btn" 
-          disabled={selectedAnswer === null}
-          >
-            <p>Summary</p>
-          </button>
-        ) : (
-          <button
-            className="next-btn"
-            onClick={goToNextQuestion}
-            disabled={selectedAnswer === null}
-          >
-            <p>Next</p>
-          </button>
-        )}
-      </div>
-      <div className="progress-wrapper">
+              Next
+            </button>
+          </div>
+          <div className="progress-wrapper">
         <progress
           className="progress-bar"
           value={(progressValue / progressMax) * 100} // Set value as a percentage
@@ -104,6 +112,8 @@ export const CurrentQuestionZustand = () => {
           {`${progressValue}/${progressMax}`}
         </div>
       </div>
-    </div>
+        </section>
+      )}
+    </>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,15 @@
 export const Footer = () => {
-  // link to our github and show the names
-  return <div>Footer</div>;
-};
+
+
+  return (
+      <footer>
+          <p>Project created by:</p>
+
+          <a href="https://github.com/VAstrom" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" />Veronica Åström</a>
+          <a href="https://github.com/MikoZhu" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" />Huadan Zhu</a>
+          <a href="https://github.com/catfooo" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" />So Youn Choi</a>
+          <a href="https://github.com/Sheryll-Ann" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" />Sheryll Poo-Cheong</a>
+
+      </footer>
+  )
+}
