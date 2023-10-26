72b06d37f21ea1dcb933735eaed0e5cc5a2854a7
Julia Holm <juliaholm@mbpsomthorjulia.home>
2023-10-26 19:57:28 +0200

    added netlify.toml file for error page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,26 +4,12 @@ import { SummaryPage } from "../pages/SummaryPage/SummaryPage";
 import { ErrorPage } from "../pages/ErrorPage/ErrorPage";
 import { QuestionPage } from "../pages/QuestionPage/QuestionPage";
 
-// export const routes = (
-//   <>
-//     {/* Page Routes */}
-//     <Route path="/" element={<StartPage />} />
-//     <Route path="/question-page" element={<QuestionPage />} />
-//     <Route path="/summary-page" element={<SummaryPage />} />
-//     <Route path="*" element={<ErrorPage />} />
-//   </>
-// );
-
-
-// Define the routes for the application.
 export const routes = (
   <>
-
-    <Route path="/" element={<StartPage />}></Route>
-
-    <Route path="/question-page" element={<QuestionPage />}></Route>
-    <Route path="/summary-page" element={<SummaryPage />}></Route>
-
-    <Route path="*" element={<ErrorPage />}></Route>
+    {/* Page Routes */}
+    <Route path="/" element={<StartPage />} />
+    <Route path="/question-page" element={<QuestionPage />} />
+    <Route path="/summary-page" element={<SummaryPage />} />
+    <Route path="*" element={<ErrorPage />} />
   </>
 );
