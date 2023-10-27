096de8ffef9663af8a4a9175f19a787c2498f133
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-27 10:29:34 +0200

    created footer component and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,23 @@
+import "../styling/footer.css"
 export const Footer = () => {
 
-
   return (
-      <footer>
-          <p>Project created by:</p>
+    <footer>
 
-          <a href="https://github.com/VAstrom" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" />Veronica Åström</a>
-          <a href="https://github.com/MikoZhu" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" />Huadan Zhu</a>
-          <a href="https://github.com/catfooo" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" />So Youn Choi</a>
-          <a href="https://github.com/Sheryll-Ann" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" />Sheryll Poo-Cheong</a>
+      <p>Project created by:</p>
 
-      </footer>
+      <div className="github-links">
+        <div>
+          <a href="https://github.com/VAstrom" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" /> Veronica Åström </a>
+          <a href="https://github.com/catfooo" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" /> So Youn Choi </a>
+        </div>
+
+        <div>
+          <a href="https://github.com/MikoZhu" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" /> Huadan Zhu </a>
+          <a href="https://github.com/Sheryll-Ann" target="_blank" rel="noopener noreferrer"><img src="./icons8-github.png" alt="Github icon" /> Sheryll P-Cheong </a>
+        </div>
+
+      </div>
+    </footer>
   )
 }
