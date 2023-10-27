70926cdd7d7e5d5001efbabf5f5057a89ea07ef3
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-27 11:29:55 +0200

    removed pointer events from questions that have been answered

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -2,59 +2,44 @@ import { Link } from "react-router-dom";
 import useQuestions from "../../stores/useQuestions";
 import useQuizStore from "../../stores/useQuestions";
 import "./board.css";
-import { useEffect, useState } from "react";
 
 export const Board = () => {
-  const [isDisabled, setIsDisabled] = useState(false);
   const { answers } = useQuizStore();
 
-  // const checker = (qId) => {
-  //   // setIsDisabled(true);
-  //   answers.map((answer) => {
-  //     if (answer.questionId === qId) {
-  //       console.log("YES", answer.questionId, qId);
-  //       setIsDisabled(true);
-  //     } else {
-  //       setIsDisabled(false);
-  //     }
-  //     // if (answer.questionId === qId && answer.isCorrect === true) {
-  //     //   console.log("YES", answer.questionId, qId);
-  //     // }
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   // answers.map((answer) => console.log(answer.questionId, answer.isCorrect));
-  //   checker();
-  // }, [answers]);
-  // console.log(answers);
+  console.log(answers);
   const questions = useQuestions((state) => state.questions);
 
   return (
     <>
       <div className="board-container">
-        {questions.map((question) => (
-          <Link
-            key={question.id}
-            to={`/quest/${question.id}`}
-            disabled={isDisabled}
-          >
-            <div className="a-question" key={question.id}>
-              <p>{question.id}</p>
+        {questions.map((question) => {
+          const hasAnswer = answers.some(
+            (answer) => answer.questionId === question.id
+          );
 
-              {answers.length > 0 &&
-                answers.map((answer) =>
-                  answer.questionId === question.id ? (
-                    answer.isCorrect === true ? (
-                      <div className="circle"></div>
-                    ) : (
-                      <div className="x">❌</div>
-                    )
-                  ) : null
-                )}
-            </div>
-          </Link>
-        ))}
+          return (
+            <Link
+              key={question.id}
+              to={`/quest/${question.id}`}
+              style={{ pointerEvents: hasAnswer ? "none" : "" }}
+            >
+              <div className="a-question" key={question.id}>
+                <p>{question.id}</p>
+
+                {answers.length > 0 &&
+                  answers.map((answer) =>
+                    answer.questionId === question.id ? (
+                      answer.isCorrect === true ? (
+                        <div className="circle"></div>
+                      ) : (
+                        <div className="x">❌</div>
+                      )
+                    ) : null
+                  )}
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </>
   );
