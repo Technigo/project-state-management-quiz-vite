41d8a60ae7932f1938ef23d9ecf5ed969b1d66c6
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-29 19:39:17 +0100

    removed routing file

--- src/routes/routeCollection.jsx
+++ src/routes/routeCollection.jsx
@@ -6,9 +6,17 @@ import { Route } from "react-router-dom";
 
 export const routeCollection = () => {
   <>
+    {/* Route to the Home page */}
+
     <Route path="/" element={<HomePage />} />
+    {/* This route leads to the element that displays when a user has entered an incorrect link/address */}
+
     <Route path="*" element={<PageNotFound />} />
+    {/* Dynamic route */}
+
     <Route path="/quest/:id" element={<QuestionPage />} />
+    {/* Route to the summary page */}
+
     <Route path="/summary" element={<SummaryPage />} />
   </>;
 };
