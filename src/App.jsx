47ed67ee6a75a93e5ef203c94334a3c05261abaa
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-29 19:47:29 +0100

    changed import

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { routeCollection } from "./routes/routeCollection";
 export const App = () => {
   return (
     <BrowserRouter>
+      {/* Routes are taken from a list inside routeCollection */}
       <Routes>{routeCollection}</Routes>
     </BrowserRouter>
   );
