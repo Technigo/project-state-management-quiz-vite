9aa65c079e3ce90ed4295fa937803885aa81054a
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-29 18:03:34 +0100

    attempted import update because of deployment errors

--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -1,5 +1,5 @@
 import { Route } from "react-router-dom";
-import { Home } from "../pages/Home";
+import { Home } from "../pages/Home.jsx";
 import { QuestionPage } from "../pages/QuestionPage";
 import { Summary } from "../pages/SummaryPage";
 import { PageNotFound } from "../pages/pagenotfound/PageNotFound";
