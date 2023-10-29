727d514b5fe726465e74007eb1e391d50350ba6f
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-29 17:48:29 +0100

    changed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import { routing } from "./routes/routing";
-// import { CurrentQuestionZustand } from "./components/CurrentQuestionZustand";
 
 export const App = () => {
   return (
--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -1,16 +1,15 @@
 import { Route } from "react-router-dom";
-import { Home } from "../pages/Home";
 import { QuestionPage } from "../pages/QuestionPage";
 import { Question } from "../pages/SummaryPage";
 import { PageNotFound } from "../pages/pagenotfound/PageNotFound";
-
+import { Home } from "../pages/Home";
 export const routing = (
   <>
     <Route path="/" element={<Home />} />
     {/* Dynamic route */}
     <Route path="/quest/:id" element={<QuestionPage />} />
     <Route path="/summary" element={<Question />} />
-    {/* This route leads to the  element that displays when a user has entered an incorrect link/address */}
+    {/* This route leads to the element that displays when a user has entered an incorrect link/address */}
     <Route path="*" element={<PageNotFound />} />
   </>
 );
