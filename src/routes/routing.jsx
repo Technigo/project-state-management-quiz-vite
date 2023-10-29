9b66b571d0d3753bbeed0805fffe45a7da99ef2c
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-29 17:44:22 +0100

    changed import

--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -7,6 +7,7 @@ import { PageNotFound } from "../pages/pagenotfound/PageNotFound";
 export const routing = (
   <>
     <Route path="/" element={<Home />} />
+    {/* Dynamic route */}
     <Route path="/quest/:id" element={<QuestionPage />} />
     <Route path="/summary" element={<Question />} />
     {/* This route leads to the  element that displays when a user has entered an incorrect link/address */}
