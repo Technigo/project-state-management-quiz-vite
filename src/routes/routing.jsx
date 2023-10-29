71cb756af8aa9adf8a046b31f5dcb69ccb9a1439
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-29 19:25:31 +0100

    changed import again

--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -1,8 +1,8 @@
 import { Route } from "react-router-dom";
-import { SummaryPage } from "../pages/summarypage/SummaryPage";
 import { HomePage } from "../pages/homepage/HomePage";
 import { PageNotFound } from "../pages/pagenotfound/PageNotFound";
 import { QuestionPage } from "../pages/questionpage/QuestionPage";
+import { SummaryPage } from "../pages/summarypage/SummaryPage";
 
 export const routing = (
   <>
@@ -10,7 +10,7 @@ export const routing = (
     <Route path="/" element={<HomePage />} />
     {/* Dynamic route */}
     <Route path="/quest/:id" element={<QuestionPage />} />
-    {/* Route to summary page */}
+    {/* Route to the summary page */}
     <Route path="/summary" element={<SummaryPage />} />
     {/* This route leads to the element that displays when a user has entered an incorrect link/address */}
     <Route path="*" element={<PageNotFound />} />
