40e34d663b49193757471d9d65f9cad49e4c95a6
majoh23 <139242342+majoh23@users.noreply.github.com>
2023-10-29 22:32:33 +0100

    Timer test 5

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -1,42 +1,33 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
-import useQuestions from "../../stores/useQuestions"; // Adjust the path accordingly
+import useQuestions from "../../stores/useQuestions";
 import useQuizStore from "../../stores/useQuestions";
-
 import { Timer } from "../Timer";
-
 import "./questions.css";
 
 export const Questions = ({ param }) => {
   const [showImage, setShowImage] = useState(true);
   const [answerIndex, setAnswerIndex] = useState(null);
-
   const navigate = useNavigate();
   const { submitAnswer } = useQuizStore();
-
   const questions = useQuestions((state) => state.questions);
   const question = questions[param - 1];
   const qImageURL = question.qImage;
   const qOptions = question.options;
-
   const timerInterval = 3000;
+
   const flipCard = () => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setShowImage(!showImage);
     }, timerInterval);
+    return () => clearTimeout(timer);
   };
+
+  useEffect(flipCard, []);
 
   const handleOptionChange = (event) => {
     setAnswerIndex(Number(event.target.value));
   };
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   submitAnswer(question.id, answerIndex);
-  //   navigate("/");
-  // }
-
-  useEffect(() => flipCard, []);
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
