37265bf89a81a653a8c284bc8017a711cb67d54b
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-29 18:18:37 +0100

    updated routing imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import { routing } from "./routes/routing";
-import { Home } from "./pages/Home";
-import { Summary } from "./pages/SummaryPage";
-import { PageNotFound } from "./pages/pagenotfound/PageNotFound";
-import { QuestionPage } from "./pages/QuestionPage";
 
 export const App = () => {
   return (
