d8e32bcaede15c34a605f94832d35922a4581e98
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-29 18:12:11 +0100

    another import update bites the dust

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { BrowserRouter, Routes } from "react-router-dom";
-import { routing } from "./routes/routing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { routing } from "./routes/routing";
+import { Home } from "./pages/Home";
+import { Summary } from "./pages/SummaryPage";
+import { PageNotFound } from "./pages/pagenotfound/PageNotFound";
+import { QuestionPage } from "./pages/QuestionPage";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routes>{routing}</Routes>
+      <Routes>
+        {/* {routing} */}
+        <Route path="/" element={<Home />} />
+        <Route path="/quest/:id" element={<QuestionPage />} />
+        <Route path="/summary" element={Summary} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 };
